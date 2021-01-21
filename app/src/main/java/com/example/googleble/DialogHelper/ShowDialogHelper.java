c3659f841d10e0cda7303d64c3644d4ea6c8e32c
VinayTS <vinay@succorfish.com>
2021-01-21 14:27:03 +0530

    Showing Dialog on click of connect and there is no bluetooth connection.

--- app/src/main/java/com/example/googleble/DialogHelper/ShowDialogHelper.java
+++ app/src/main/java/com/example/googleble/DialogHelper/ShowDialogHelper.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.example.googleble.R;
 
 
@@ -22,7 +24,7 @@ public class ShowDialogHelper {
                     if (mAlertDialog != null && mAlertDialog.isShowing()) {
 
                     } else {
-                        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(mActivity, R.style.AppCompatAlertDialogStyle);
+                      /*  android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(mActivity, R.style.AppCompatAlertDialogStyle);
                         builder.setMessage(message).setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int id) {
@@ -30,10 +32,45 @@ public class ShowDialogHelper {
                             }
                         });
                         mAlertDialog = builder.create();
+                        mAlertDialog.show();*/
+
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                        builder.setMessage(message).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        mAlertDialog = builder.create();
                         mAlertDialog.show();
+
                     }
+
+
+
+
                 }
             });
         }
+    }
+
+    public void e(Activity activity){
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage("")
+                .setPositiveButton("s", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("a", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+                builder.create();
+                builder.show();
+
     }
 }
